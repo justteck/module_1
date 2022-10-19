ea45dd6015eccbec1cc3a91d52baed1ce4f8a6f2
justteck <justteck@yandex.ru>
2022-10-19 17:14:30 +0300

    folder was added

---
+++ lesson_01/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const product = 'bread';
+const productQuantity = 10;
+const productCategory = 'food';
+const productPrice = 50;
+
+console.log('Product name:', product);
+console.log('Total price:', productQuantity * productPrice);
