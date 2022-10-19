4d0609604901cdaf20bcc8b64ce0c2aab8e1f92c
justteck <justteck@yandex.ru>
2022-10-19 17:19:33 +0300

    new product was added

--- lesson_01/script.js
+++ lesson_01/script.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const product = 'bread';
-const productQuantity = 10;
-const productCategory = 'food';
-const productPrice = 50;
+{
+  const product = 'bread';
+  const productQuantity = 10;
+  const productCategory = 'food';
+  const productPrice = 50;
 
-console.log('Product name:', product);
-console.log('Total price:', productQuantity * productPrice);
+  console.log('Product name:', product);
+  console.log('Total price:', productQuantity * productPrice);
+}
+
+{
+  const product = 'butter';
+  const productQuantity = 40;
+  const productCategory = 'food';
+  const productPrice = 10;
+
+  console.log('Product name:', product);
+  console.log('Total price:', productQuantity * productPrice);
+}
+
