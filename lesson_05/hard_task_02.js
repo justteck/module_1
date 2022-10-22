ff1a5683594826b62778172a3d88b9c450d40073
justteck <justteck@yandex.ru>
2022-10-22 11:11:02 +0300

    added lesson_05 hard_task_02

--- lesson_05/hard_task_02.js
+++ lesson_05/hard_task_02.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const a = +prompt('Введите первое число', 5);
-const b = +prompt('Введите второе число', 5);
+// const a = +prompt('Введите первое число', 5);
+// const b = +prompt('Введите второе число', 5);
 
 const findMaxNumber = (firstNum, secondNum) => {
   const a = firstNum;
   const b = secondNum;
 
-  const sign = Math.sign(a - b);
-}
+  return (Number(a > b) * a) + (Number(b > a) * b);
+};
+
+console.log('Большее число: ', findMaxNumber(101, 222));
